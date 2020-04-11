9b1a5cf650f462d62a7c06d3c27161444e8f0773
Matt Ingham <matt.g.ingham@gmail.com>
2020-04-11 20:28:38 +0100

    Cat image commit

--- src/app/map-base/map-base.component.ts
+++ src/app/map-base/map-base.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { element } from 'protractor';
-import { Countries } from '../app.countries.model';
 import { CountriesData } from '../countries-data.model';
-//import { NameMe } from './name-me.model';
 
 @Component({
     selector: 'app-map-base',
@@ -11,88 +8,34 @@ import { CountriesData } from '../countries-data.model';
 })
 
 export class MapBaseComponent implements OnInit {
-
+    
+    //Object contraining countries data.
+    cntryData: CountriesData = new CountriesData();
+    //DOM manipulation and country data variables.
     isActive = false;
     countryName: string;
     currentId: string;
     currentIndex: number;
-    answer: string;
-    isCorrect: boolean;
-    isCorrectText: string;
-    points: number = 0;
-    finalPoints: number;
-    timer: number = 180;
-    //finalAnswer: string;
-    activeRadio: number;
-    d: any;
-    cntryData: CountriesData = new CountriesData();
-    //cd: CountriesData[] = [];
-    
-
-    
- 
-    ac: any[] = new Countries().allCountries;
-    answerOptionsArray = [""];
-    opts: any[];
-
-    correctAnswer: string;
-    wrongAnswerOne: string;
-    wrongAnswerTwo: string;
-    
-
     startDisabled = false;
     goDisabled = false;
     gameOver: boolean = false;
-
-    //nameMe: NameMe = new NameMe(0,180)
-
-    /*selectCountry(event: any){
-
-        let startString: string;
-        let subString: string;
-        let allCountries;
-
-        let cntryIndex;
-        
-        if(this.currentId){
-            document.getElementById(this.currentId).classList.remove('active');
-        }
-        event.target.classList.add('active');
-        this.currentId = event.target.getAttribute('id');
-
-       cntryIndex = this.cntryData.cntryObj["countries"].indexOf('Germany')
-
-        alert(this.cntryData.cntryObj["countries"][0].Xpos);
-
-        allCountries = document.getElementsByClassName("country");
-
-      for(let i = 0; i < allCountries.length; i ++){
-
-            let cData = new CountriesData();
-            let countryId;
-            let startingXpos;
-            let startingYpos;
-            let countryName;
-
-            startString = allCountries[i].getAttribute('d');
-            countryId = allCountries[i].getAttribute('id');
-            countryName = allCountries[i].getAttribute('data-name');
-            subString = startString.substring(2,15);
-            startingXpos = subString.substring(0,subString.indexOf(','));
-            startingYpos = subString.substring(subString.indexOf(',') + 1,subString.indexOf(' '));
-            
-            cData.countryName = "{'Name': " + "'" + countryName + "',";
-            cData.countryId = "'Id': " + "'" + countryId + "',";
-            cData.dataId = "'DataId': " + i + ",";
-            cData.xStartPos = "'Xpos': " + startingXpos + ",";
-            cData.yStartPos = "'Ypos': " + startingYpos + ",";
-            cData.pathData = "'PData': " + '"' + startString + '"},';
-
-            this.cd.push(cData);
-        }
-
-    }*/
-
+    x: number;
+    y: number;
+    //Scoring variables.
+    points: number = 0;
+    finalPoints: number;
+    //Answers and options variables
+    answer: string;
+    isCorrect: boolean;
+    isCorrectText: string;
+    activeRadio: number;
+    answerOptionsArray = [""];
+    opts: any[];
+    correctAnswer: string;
+    wrongAnswerOne: string;
+    wrongAnswerTwo: string;
+    //Timer variables
+    timer: number = 180;
 
     startTimer(){
 
@@ -120,30 +63,21 @@ export class MapBaseComponent implements OnInit {
         this.opts = [];
         this.answerOptionsArray = [];
         this.countryName = "";
-        //let c = document.getElementsByClassName("country");
-        //let d = document.getElementById(this.currentId);
-        //let arrLength = this.ac.length;
-        let rndNumOne = Math.floor(Math.random() * (this.ac.length-1+1)) + 1;
-        let rndNumTwo = Math.floor(Math.random() * (this.ac.length-1+1)) + 1;
+
+        let countries = this.cntryData.cntryObj['countries'];
+        let rndNumOne = Math.floor(Math.random() * (countries.length-1+1)) + 1;
+        let rndNumTwo = Math.floor(Math.random() * (countries.length-1+1)) + 1;
         let rdmCntryId = Math.floor(Math.random() * (220-1+1)) + 1;
 
-        /*if(this.currentId){
-            d.classList.remove('active');
-        }*/
-
-        /*if(this.currentIndex){
-            c[this.currentIndex].classList.remove('active');
-        }*/
-
-        //c[rdmCntryId].classList.add('active');
-
         this.currentIndex = rdmCntryId;
-        //this.currentId = c[rdmCntryId].id;
 
-        this.answerOptionsArray.push(this.ac[rndNumOne]);
-        this.answerOptionsArray.push(this.ac[rndNumTwo]);
-        this.answerOptionsArray.push(this.cntryData.cntryObj['countries'][this.currentIndex].Name);
+        this.answerOptionsArray.push(countries[rndNumOne].Name);
+        this.answerOptionsArray.push(countries[rndNumTwo].Name);
+        this.answerOptionsArray.push(countries[this.currentIndex].Name);
         this.shuffle(this.answerOptionsArray);
+
+        this.x = countries[this.currentIndex].Xpos + 5;
+        this.y = countries[this.currentIndex].Ypos + 5;
 
         this.opts = this.answerOptionsArray;
 
@@ -184,7 +118,6 @@ export class MapBaseComponent implements OnInit {
     }
 
     restartGame(){
-        //document.getElementById("end-game-container").style.display = "none";  
         this.gameOver = false;
         this.startDisabled = false;
         this.goDisabled = false; 
